254ad21ed4fd9aaa6d94eb9f400a64869907e9fc
Frank <fha@easv.dk>
2024-08-23 12:15:27 +0200

    Add project files.

---
+++ WpfDaisyDuck/Duck.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDaisyDuck
+{
+    public delegate void SpeakDelegate(string speak);
+
+    public class Duck
+    {
+        private string name;
+        public event SpeakDelegate speakDel;
+        Random rnd = new Random();
+
+        public Duck(String name)
+        {
+            this.name = name;
+        }
+
+        public void StandUp(string sound)
+        {
+            if (sound.ToLower().Equals("standup"))
+            {
+                Debug.WriteLine($"{name} I will stand up");
+            }
+        }
+
+        public void Listening(string sound)
+        {
+            //Print what the Duck is hearing
+            //Debug.WriteLine(this.name + " - " + sound);
+            String say = rnd.Next(0, 10) % 2 == 0 ? "Yes" : "No";
+            Debug.WriteLine($"{this.name} answers {say} to question: {sound}");
+        }
+
+        public void Speak()
+        {
+            //Print what the Duck is saying
+            Debug.WriteLine("Daisy asks a question");
+            //this.speakDel?.Invoke("Question (Y/N)");
+            if(this.speakDel!=null)
+                this.speakDel("Question (Y/N)");
+        }
+    }
+}
---
+++ WpfDaisyDuck/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfDaisyDuck
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //Duck Instance variables
+        Duck daisy = null;
+        Duck huey = null;
+        Duck duey = null;
+        Duck louie = null;
+        Duck donald = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //instantiate Duck variables
+            daisy = new Duck("Daisy");
+            huey = new Duck("Huey");
+            duey = new Duck("Duey");
+            louie = new Duck("Louie");
+            donald = new Duck("Donald");
+
+            daisy.speakDel += huey.Listening;
+            daisy.speakDel += duey.Listening;
+            daisy.speakDel += louie.Listening;
+
+            daisy.speakDel += donald.StandUp;
+
+            //Add Lambda eventhandler to daisy button, programmatically
+            btnDaisy.Click -= btnDaisy_Click;
+            btnDaisy.Click += (s,e) => daisy.Speak();
+
+        }
+
+        private void btnDaisy_Click(object sender, RoutedEventArgs e)
+        {
+            //Make Daisy ask
+            daisy.Speak();
+
+        }
+    }
+}
